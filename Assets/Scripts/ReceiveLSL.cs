830f4e2fea60a6ebad8bfce602782e9de294d775
Rohan Modi <rohanmodi@outlook.in>
2025-07-23 16:52:43 -0400

    add lsl receiver, avatar outlet

---
+++ Assets/Scripts/ReceiveLSL.cs
@@ -0,0 +1,87 @@
+﻿using LSL;
+using System.Collections;
+using UnityEngine;
+using XCharts.Runtime;
+
+namespace LSL4Unity.Samples.SimpleInlet
+{
+    public class SimpleInletScaleObject : MonoBehaviour
+    {
+        public string StreamName;
+        public LineChart chart;
+
+        private ContinuousResolver resolver;
+
+        private StreamInlet inlet;
+        private float[] sample_buffer;
+        private int n_channels;
+
+        void Start()
+        {
+            if (!string.IsNullOrEmpty(StreamName))
+            {
+                Debug.Log($"LSL Stream- starting input for stream \"{StreamName}\"");
+                resolver = new ContinuousResolver("name", StreamName);
+            }
+            else
+            {
+                this.enabled = false;
+                return;
+            }
+
+            StartCoroutine(ResolveExpectedStream());
+        }
+
+        IEnumerator ResolveExpectedStream()
+        {
+            var results = resolver.results();
+
+            while (results.Length == 0)
+            {
+                yield return new WaitForSeconds(0.1f);
+                results = resolver.results();
+            }
+
+            Debug.Log($"LSL Stream- \"{StreamName}\" found.");
+            inlet = new StreamInlet(results[0]);
+
+            n_channels = inlet.info().channel_count();
+            sample_buffer = new float[n_channels];
+
+            Debug.Log($"LSL Stream- stream info: {n_channels} channels.");
+        }
+
+        void Update()
+        {
+            if (inlet != null)
+            {
+                double timestamp = inlet.pull_sample(sample_buffer, 0.0);
+
+                if (timestamp != 0.0)
+                {
+                    if (n_channels >= 3)
+                    {
+                        float x = sample_buffer[0];
+                        float y = sample_buffer[1];
+                        float z = sample_buffer[2];
+
+                        chart.AddData(0, x);
+
+                        Vector3 new_scale = new Vector3(x, y, z);
+                        gameObject.transform.localScale = new_scale;
+
+                        Debug.Log($"LSL Stream- updated object scale to: {new_scale}");
+                    }
+                    else
+                    {
+                        Debug.LogWarning($"LSL Stream- not enough channels- found {n_channels}, need >= 3");
+                    }
+                }
+            }
+            else
+            {
+                Debug.LogWarning("LSL Stream- no inlet connected yet");
+            }
+        }
+    }
+}
--- Assets/Scripts/VRButtonLSLOutlet.cs
+++ Assets/Scripts/VRButtonLSLOutlet.cs
@@ -1,9 +1,14 @@
 using LSL;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using XCharts;
+using XCharts.Runtime;
 
 public class VRButtonLSLOutlet : MonoBehaviour
 {
@@ -13,12 +18,36 @@ public class VRButtonLSLOutlet : MonoBehaviour
     string StreamType = "Markers";
     private StreamOutlet outlet;
     private StreamOutlet outlet2;
+    private StreamOutlet outlet3;
     private string[] sample = { "" };
-    private float[] positionSample = new float[9];
+    private double[] positionSample = new double[10];
 
     private Transform headTransform;
     private Transform leftHandTransform;
     private Transform rightHandTransform;
+
+    private double[] avatarSample = new double[46]; // 15 joints * 3 + 1 timestamp
+    private readonly OVRSkeleton.BoneId[] trackedBones = new OVRSkeleton.BoneId[]
+    {
+        OVRSkeleton.BoneId.Body_Head,
+        OVRSkeleton.BoneId.Body_Neck,
+        OVRSkeleton.BoneId.Body_LeftShoulder,
+        OVRSkeleton.BoneId.Body_RightShoulder,
+        OVRSkeleton.BoneId.Body_SpineLower,
+        OVRSkeleton.BoneId.Body_SpineMiddle,
+        OVRSkeleton.BoneId.Body_Hips,
+        OVRSkeleton.BoneId.FullBody_LeftUpperLeg,
+        OVRSkeleton.BoneId.FullBody_RightUpperLeg,
+        OVRSkeleton.BoneId.FullBody_LeftLowerLeg,
+        OVRSkeleton.BoneId.FullBody_RightLowerLeg,
+        OVRSkeleton.BoneId.FullBody_LeftFootAnkle,
+        OVRSkeleton.BoneId.FullBody_RightFootAnkle,
+        OVRSkeleton.BoneId.FullBody_LeftHandWrist,
+        OVRSkeleton.BoneId.FullBody_RightHandWrist
+    };
+    private OVRSkeleton bodySkeleton;
+
+
 
     private void Awake()
     {
@@ -50,9 +79,19 @@ public class VRButtonLSLOutlet : MonoBehaviour
         hash2.Append("PositionStream");
         hash2.Append(gameObject.GetInstanceID());
 
-        StreamInfo streamInfo2 = new StreamInfo("VRPositionStream", "PositionStream", 9, LSL.LSL.IRREGULAR_RATE, channel_format_t.cf_float32, hash2.ToString());
+        StreamInfo streamInfo2 = new StreamInfo("VRPositionStream", "PositionStream", 10, LSL.LSL.IRREGULAR_RATE, channel_format_t.cf_double64, hash2.ToString());
         outlet2 = new StreamOutlet(streamInfo2);
         //Debug.Log("LSL position stream created: " + "VRPositionStream");
+
+
+        var hash3 = new Hash128();
+        hash2.Append("VRAvatarPositionStream");
+        hash2.Append("AvatarPositionStream");
+        hash2.Append(gameObject.GetInstanceID());
+
+        StreamInfo streamInfo3 = new StreamInfo("VRAvatarPositionStream", "AvatarPositionStream", 46, LSL.LSL.IRREGULAR_RATE, channel_format_t.cf_double64, hash3.ToString());
+        outlet3 = new StreamOutlet(streamInfo3);
+
     }
 
     void Update()
@@ -63,19 +102,55 @@ public class VRButtonLSLOutlet : MonoBehaviour
             Vector3 l = leftHandTransform.position;
             Vector3 r = rightHandTransform.position;
 
-            positionSample[0] = h.x;
-            positionSample[1] = h.y;
-            positionSample[2] = h.z;
+            positionSample[0] = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
 
-            positionSample[3] = l.x;
-            positionSample[4] = l.y;
-            positionSample[5] = l.z;
+            positionSample[1] = h.x;
+            positionSample[2] = h.y;
+            positionSample[3] = h.z;
 
-            positionSample[6] = r.x;
-            positionSample[7] = r.y;
-            positionSample[8] = r.z;
+            positionSample[4] = l.x;
+            positionSample[5] = l.y;
+            positionSample[6] = l.z;
+
+            positionSample[7] = r.x;
+            positionSample[8] = r.y;
+            positionSample[9] = r.z;
 
             outlet2?.push_sample(positionSample);
+
+            if (bodySkeleton != null &&
+                bodySkeleton.IsInitialized &&
+                bodySkeleton.Bones != null &&
+                bodySkeleton.Bones.Count > 0 &&
+                bodySkeleton.IsDataValid &&
+                bodySkeleton.IsDataHighConfidence)
+            {
+                avatarSample[0] = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+                int idx = 1;
+
+                foreach (var boneId in trackedBones)
+                {
+                    OVRBone bone = bodySkeleton.Bones
+                        .FirstOrDefault(b => b.Id == boneId);
+
+                    if (bone != null && bone.Transform != null)
+                    {
+                        Vector3 p = bone.Transform.position;
+                        avatarSample[idx++] = p.x;
+                        avatarSample[idx++] = p.y;
+                        avatarSample[idx++] = p.z;
+                    }
+                    else
+                    {
+                        avatarSample[idx++] = 0;
+                        avatarSample[idx++] = 0;
+                        avatarSample[idx++] = 0;
+                    }
+                }
+
+                outlet3?.push_sample(avatarSample);
+            }
+
         }
     }
 
@@ -101,6 +176,10 @@ public class VRButtonLSLOutlet : MonoBehaviour
         headTransform = GameObject.Find("CenterEyeAnchor")?.transform;
         leftHandTransform = GameObject.Find("LeftHandAnchor")?.transform;
         rightHandTransform = GameObject.Find("RightHandAnchor")?.transform;
+
+        bodySkeleton = FindObjectOfType<OVRSkeleton>();
+        int jointCount = trackedBones.Length;
+        avatarSample = new double[1 + jointCount * 3];
     }
 
     private void OnDestroy()
